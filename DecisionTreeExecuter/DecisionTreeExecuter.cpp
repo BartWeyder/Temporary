155297b2a4e827c7c5b54a94f62cd77939a3bfaa
BartWeyder <jackjunior98@gmail.com>
2019-11-10 20:09:57 +0200

    Decision Trees: Change executer interface and provide export of tree test
    Provided changes:
    1. Changed Executer interface so it now takes 3 parameters:
    test data set length, path to data set file and path to output file.
    2. Changed VS settings for tabs - now it's 3 spaces.
    3. Fixed builds for Release and both CPU architectures.

--- DecisionTreeExecuter/DecisionTreeExecuter.cpp
+++ DecisionTreeExecuter/DecisionTreeExecuter.cpp
@@ -8,59 +8,76 @@
 #include "DataUtils.h"
 
 /// @brief Executable to run from console.
-/// @params Works in 2 ways: 
-///          - with no params - standart values will be used;
-///          - with 2 params: training data length and path to CSV file.
+/// @params
+///          1) Training data length - how many rows we take to train tree.
+///          2) Path to data set file (in .csv format).
+///          3) Path to save recognition results (with file name).
 int main(int argc, char **argv)
 {
-	if (argc != 1 && argc != 3) return -1;
+   if (argc > 4) return -1;
 
-	constexpr auto standartTrainingDataLen = 15;
-	constexpr auto standartCSVFile = "MP-06-Kizim.csv";
-	const auto trainingDataLen = argc == 0 ? standartTrainingDataLen : std::stoi(argv[1]);
-	const auto CSVFile = argc == 0 ? standartCSVFile : argv[2];
+   // Default values
+   constexpr auto standartTrainingDataLen = 15;
+   constexpr auto standartCSVFile = "MP-06-Kizim.csv";
+   constexpr auto standartOutputFile = "TreeRecognitionResult.csv";
 
-	rapidcsv::Document doc(CSVFile, rapidcsv::LabelParams(-1, -1), rapidcsv::SeparatorParams(';'));
-	Examples fullSet(doc.GetColumnCount());
-	Examples trainingExamples(doc.GetColumnCount());
-	for (size_t i = 0; i < trainingDataLen; ++i)
-	{
-		InsertRow(trainingExamples, doc.GetRow<std::string>(i));
-		InsertRow(fullSet, doc.GetRow<std::string>(i));
-	}
-	Examples checkExamples(doc.GetColumnCount());
-	for (size_t i = trainingDataLen; i < doc.GetRowCount(); i++)
-	{
-		InsertRow(checkExamples, doc.GetRow<std::string>(i));
-		InsertRow(fullSet, doc.GetRow<std::string>(i));
-	}
+   // Values to use
+   const auto trainingDataLen = argc >= 2 ? std::stoi(argv[1]) : standartTrainingDataLen;
+   const auto CSVFile = argc >= 3 ? argv[2] : standartCSVFile;
+   const auto outputFileName = argc >= 4 ? argv[3] : standartOutputFile;
 
-	// Prepare attributes "header" list.
-	Attributes i_attributes;
-	for (size_t i = 0; i < doc.GetColumnCount() - 1; i++)
-	{
-		i_attributes.insert(i);
-	}
+   // Read file
+   rapidcsv::Document doc(CSVFile, rapidcsv::LabelParams(-1, -1), rapidcsv::SeparatorParams(';'));
 
-	// Prepare all unique attribute values.
-	UniqueAttributeValues map;
-	for (const auto attrIdx : i_attributes)
-	{
-		map.insert({ attrIdx, UniqueValues(fullSet[attrIdx].begin(), fullSet[attrIdx].end()) });
-	}
-	const auto uniqueAttributeValues = std::make_shared<const UniqueAttributeValues>(std::move(map));
+   Examples fullSet(doc.GetColumnCount()); // Full data set
+   Examples trainingExamples(doc.GetColumnCount()); // Only first n rows for training purposes
+   for (size_t i = 0; i < trainingDataLen; ++i)
+   {
+      InsertRow(trainingExamples, doc.GetRow<std::string>(i));
+      InsertRow(fullSet, doc.GetRow<std::string>(i));
+   }
+   Examples checkExamples(doc.GetColumnCount()); // Rest rows after training one to test tree. 
+   for (size_t i = trainingDataLen; i < doc.GetRowCount(); i++)
+   {
+      InsertRow(checkExamples, doc.GetRow<std::string>(i));
+      InsertRow(fullSet, doc.GetRow<std::string>(i));
+   }
 
-	// Build tree
-	ID3Node tree{ trainingExamples, i_attributes, uniqueAttributeValues };
+   // Prepare attributes "header" list.
+   Attributes i_attributes;
+   for (size_t i = 0; i < doc.GetColumnCount() - 1; i++)
+   {
+      i_attributes.insert(i);
+   }
 
-	// Check our tree on test data set.
-	auto rightCounter = 0;
-	for (size_t i = 0; i < checkExamples.begin()->size(); ++i)
-	{
-		const auto actual = tree.GetValue(GetRow(checkExamples, i));
-		const auto expected = (*checkExamples.rbegin())[i];
-		std::cout << expected << " and should be " << actual << std::endl;
-		if (expected == actual) ++rightCounter;
-	}
-	std::cout << "Right classified: " << rightCounter << "/" << doc.GetRowCount() - trainingDataLen;
+   // Prepare all unique attribute values.
+   UniqueAttributeValues map;
+   for (const auto attrIdx : i_attributes)
+   {
+      map.insert({ attrIdx, UniqueValues(fullSet[attrIdx].begin(), fullSet[attrIdx].end()) });
+   }
+   const auto uniqueAttributeValues = std::make_shared<const UniqueAttributeValues>(std::move(map));
+
+   // Prepare output file
+   std::ofstream file(outputFileName);
+   file << "Test Case,Expected,Algorithm Output" << std::endl;
+   file.close();
+   rapidcsv::Document output(outputFileName, rapidcsv::LabelParams(0, -1));
+
+   // Build tree
+   ID3Node tree{ trainingExamples, i_attributes, uniqueAttributeValues };
+
+   // Check our tree on test data set.
+   auto rightCounter = 0;
+   for (size_t i = 0; i < checkExamples.begin()->size(); ++i)
+   {
+      const auto actual = tree.GetValue(GetRow(checkExamples, i));
+      const auto expected = (*checkExamples.rbegin())[i];
+      std::cout << expected << " and should be " << actual << std::endl;
+      if (expected == actual) ++rightCounter;
+      output.SetRow(i, Row{ std::to_string(trainingDataLen + i), expected, actual });
+   }
+
+   output.Save();
+   std::cout << "Right classified: " << rightCounter << "/" << doc.GetRowCount() - trainingDataLen;
 }
--- ID3_DecisionTree/ID3Node.cpp
+++ ID3_DecisionTree/ID3Node.cpp
@@ -9,90 +9,91 @@
 
 namespace
 {
-	/// @brief Returns most popular value in column.
-	std::string findTheMostPopular(const AttributeColumn& i_column)
-	{
-		UniqueValues uniqueValues(i_column.begin(), i_column.end());
-		ptrdiff_t maxCount = 0;
-		auto maxValue = *i_column.begin();
-		for (const auto& value : uniqueValues)
-		{
-			const auto count = std::count(i_column.begin(), i_column.end(), value);
-			if (count > maxCount)
-			{
-				maxCount = count;
-				maxValue = value;
-			}
-		}
-		return maxValue;
-	}
+   /// @brief Returns most popular value in column.
+   std::string findTheMostPopular(const AttributeColumn& i_column)
+   {
+      UniqueValues uniqueValues(i_column.begin(), i_column.end());
+      ptrdiff_t maxCount = 0;
+      auto maxValue = *i_column.begin();
+      for (const auto& value : uniqueValues)
+      {
+         const auto count = std::count(i_column.begin(), i_column.end(), value);
+         if (count > maxCount)
+         {
+            maxCount = count;
+            maxValue = value;
+         }
+      }
+      return maxValue;
+   }
 }
 
 ///////////////////////////////////////////////////////////////////////////////
 ID3Node::ID3Node(const Examples & i_examples, const Attributes & i_attributes,
-	UniqueAttributeValuesPtr i_uniqueAttributeValues)
-	: m_uniqueAttributeValues(i_uniqueAttributeValues)
+   UniqueAttributeValuesPtr i_uniqueAttributeValues)
+   : m_uniqueAttributeValues(i_uniqueAttributeValues)
 {
-	// Set unique attributes on root creation
-	if (!m_uniqueAttributeValues)
-	{
-		UniqueAttributeValues map;
-		for (const auto attrIdx : i_attributes)
-		{
-			map.insert({ attrIdx, UniqueValues(i_examples[attrIdx].begin(), i_examples[attrIdx].end()) });
-		}
+   // Set unique attributes on root creation
+   if (!m_uniqueAttributeValues)
+   {
+      UniqueAttributeValues map;
+      for (const auto attrIdx : i_attributes)
+      {
+         map.insert({ attrIdx, UniqueValues(i_examples[attrIdx].begin(), i_examples[attrIdx].end()) });
+      }
 
-		m_uniqueAttributeValues = std::make_shared<const UniqueAttributeValues>(std::move(map));
-	}
+      m_uniqueAttributeValues = std::make_shared<const UniqueAttributeValues>(std::move(map));
+   }
 
-	// first checks zone
-	if (i_attributes.empty())
-	{
-		throw std::logic_error("Attributes empty, return most popular class.");
-	}
+   // first checks zone
+   if (i_attributes.empty())
+   {
+      throw std::logic_error("Attributes empty, return most popular class.");
+   }
 
-	UniqueValues uniqueClasses(i_examples.rbegin()->begin(), i_examples.rbegin()->end());
-	if (uniqueClasses.size() <= 1)
-	{
-		throw std::logic_error("There is only one possible class, nothing to define...");
-	}
+   UniqueValues uniqueClasses(i_examples.rbegin()->begin(), i_examples.rbegin()->end());
+   if (uniqueClasses.size() <= 1)
+   {
+      throw std::logic_error("There is only one possible class, nothing to define...");
+   }
 
-	m_attribute = GetBestAttribute(i_examples, i_attributes);
-	for (const auto& attr : m_uniqueAttributeValues->at(m_attribute))
-	{
-		Examples subsetOfExamples(i_examples.size());
-		for (size_t i = 0; i < i_examples.begin()->size(); i++)
-		{
-			if (i_examples.at(m_attribute)[i] == attr)
-			{
-				InsertRow(subsetOfExamples, GetRow(i_examples, i));
-			}
-		}
+   m_attribute = GetBestAttribute(i_examples, i_attributes);
+   for (const auto& attr : m_uniqueAttributeValues->at(m_attribute))
+   {
+      Examples subsetOfExamples(i_examples.size());
+      for (size_t i = 0; i < i_examples.begin()->size(); i++)
+      {
+         if (i_examples.at(m_attribute)[i] == attr)
+         {
+            InsertRow(subsetOfExamples, GetRow(i_examples, i));
+         }
+      }
 
-		// Check if subset is empty there are only single-class examples left:
-		if (subsetOfExamples.empty())
-		{
-			m_childNodes.insert({ attr, std::make_unique<SimpleID3Node>(findTheMostPopular(*i_examples.rbegin())) });
-			continue;
-		}
+      // Check if subset is empty there are only single-class examples left:
+      if (subsetOfExamples.empty())
+      {
+         m_childNodes.insert({ attr, std::make_unique<SimpleID3Node>(findTheMostPopular(*i_examples.rbegin())) });
+         continue;
+      }
 
-		Attributes childAttributes{ i_attributes };
-		childAttributes.erase(m_attribute);
+      Attributes childAttributes{ i_attributes };
+      childAttributes.erase(m_attribute);
 
-		try
-		{
-			m_childNodes.insert({ attr, std::make_unique<ID3Node>(subsetOfExamples, childAttributes, m_uniqueAttributeValues) });
-		}
-		catch (const std::logic_error&)
-		{
-			m_childNodes.insert({ attr, std::make_unique<SimpleID3Node>(findTheMostPopular(*i_examples.rbegin())) });
-		}
-	}
+      try
+      {
+         m_childNodes.insert({ attr, std::make_unique<ID3Node>(subsetOfExamples, childAttributes,
+            m_uniqueAttributeValues) });
+      }
+      catch (const std::logic_error&)
+      {
+         m_childNodes.insert({ attr, std::make_unique<SimpleID3Node>(findTheMostPopular(*i_examples.rbegin())) });
+      }
+   }
 }
 
 ///////////////////////////////////////////////////////////////////////////////
 const std::string ID3Node::GetValue(const Example & i_experiment) const
 {
-	const auto& attribute = i_experiment[m_attribute];
-	return m_childNodes.at(attribute)->GetValue(i_experiment);
+   const auto& attribute = i_experiment[m_attribute];
+   return m_childNodes.at(attribute)->GetValue(i_experiment);
 }
